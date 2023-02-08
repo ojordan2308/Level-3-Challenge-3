1c23985f89eb1761f76d31e647083a91a4f6a248
Oliver Jordan <o.jordan2308@gmail.com>
2023-02-08 08:53:11 +0000

    Completed maxmin task

---
+++ maxmin.js
@@ -0,0 +1,17 @@
+const findMaxMin = arr => {
+    let max = arr[0]
+    let min = arr[0]
+    for (let i=1; i<arr.length; i++) {
+        if (arr[i]>max) {
+            max = arr[i]
+        }
+        if (arr[i]<min) {
+            min = arr[i]
+        }
+    }
+    return [min, max]
+}
+
+let inputArray = prompt('Input an array of numbers')
+
+console.log(findMaxMin(inputArray))
